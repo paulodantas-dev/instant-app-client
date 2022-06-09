53f170a626738def496da959d913f16c306c8037
Paulo Dantas <paulo.roberto@hdn.digital>
2022-06-09 14:30:08 -0300

    Initial Project

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import { Home } from './pages/home/Home';
+
+export const App = () => {
+  return <Home />;
+};
---
+++ src/pages/home/HomeView.tsx
@@ -0,0 +1,7 @@
+export const HomeView = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-red-500">Home</h1>
+    </div>
+  );
+};
